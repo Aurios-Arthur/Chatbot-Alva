963481e68f85221e6cf5e880a95949586ad49c3d
Arthur Silva <arthurss0305@gmail.com>
2025-03-21 14:02:17 -0300

    Feature: Inteligencia artifical treinada

---
+++ scripts.js
@@ -0,0 +1,56 @@
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+const chatbox = document.querySelector(".chatbox");
+
+let userMessage;
+const API_KEY = "AIzaSyDDFy3bKhKwUBMnv4XvZm1a42RPRnZv2XM"
+
+const createChatLi = (message, className) => {
+    //Create a chat <li> element with passed message and className
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", className);
+    let chatContent = className === "outgoing" ? `<p>${message}</p>` : `<span class="material-symbols-outlined">smart_toy</span><p>${message}</p>`;
+    chatLi.innerHTML = chatContent;
+    return chatLi; 
+}
+
+const generateResponse = (incomingChatLi) => {
+    const API_URL =  `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
+    const messageElement = incomingChatLi.querySelector("p");
+
+    const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ 
+          contents: [{ 
+            role: "user", 
+            parts: [{ text: userMessage }] 
+          }] 
+        }),
+      };
+
+    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
+        messageElement.textContent = data.candidates[0].content.parts[0].text;
+    }).catch((error) => {
+        messageElement.textContent = "Oops, Algo deu errado";
+    }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+}
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim();
+    if(!userMessage)return;
+
+    //Append the user's message to te chatbox
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+
+    setTimeout(() => {
+        //Envia menssagem de Pensando ao enviar menssagem
+        const incomingChatLi = createChatLi("Pensando...", "incoming")
+        chatbox.appendChild(incomingChatLi);
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+        generateResponse(incomingChatLi);
+    }, 600);
+}
+
+sendChatBtn.addEventListener("click", handleChat);
